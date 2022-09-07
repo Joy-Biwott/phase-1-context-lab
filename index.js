1f78bde87c282e00db205fe0b93c8a5500b8852e
Joy <biwott.joy.03@gmail.com>
2022-09-07 09:33:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
 /* Your Code Here */
+function createEmployeeRecord (array) {
+    return {
+       firstName : array[0],
+       familyName : array[1],
+       title : array[2],
+       payPerHour : array[3],
+       timeInEvents : [],
+       timeOutEvents : []
+    }
+}
+
+function createEmployeeRecords(employeeData){
+    return employeeData.map(function(array){
+        return createEmployeeRecord(array)
+    })
+}
+
+function createTimeInEvent(dateStamp){
+    let[date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type : "TimeIn",
+        hour : parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type : "TimeOut",
+        hour : parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function hoursWorkedOnDate(soughtDate) {
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate (dateSought){
+    let arrayWage = hoursWorkedOnDate.call(this, dateSought)
+        *this.payPerHour
+        return parseFloat(arrayWage.toString())
+}
+
+function allWages(){
+    let eligibleDates = this.timeInEvents.map(function(e){
+         return e.date
+    })
+    let payable = eligibleDates.reduce(function(memo, d){
+     return memo + wagesEarnedOnDate.call(this, d)
+ }.bind(this), 0)
+ 
+ return payable
+ }
+ function findEmployeeByFirstName(srcArray, firstName) {
+     return srcArray.find(function(rec){
+       return rec.firstName === firstName
+     })
+   }
+ 
+   function calculatePayroll(arrayOfEmployeeRecords){
+       return arrayOfEmployeeRecords.reduce(function(memo, rec){
+           return memo + allWagesFor.call(rec)
+       }, 0)
+   }
+ 
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
